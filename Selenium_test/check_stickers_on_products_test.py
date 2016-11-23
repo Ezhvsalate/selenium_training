802fec0d1dfe59570e2938147a5f0200d1915b8d
Dmitry Korotkov <ezhvsalate@yandex.ru>
2016-11-23 19:47:25 +0300

    Check if all products on the main page have stickers test

---
+++ Selenium_test/check_stickers_on_products_test.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Firefox(firefox_binary="c:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe")
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_click_all_adm_menu_elements(driver):
+    driver.get("http://localhost/2/")
+    elements_list = driver.find_elements_by_css_selector("li.product.column")
+    for element in elements_list:
+        sticker = element.find_element_by_css_selector("div.sticker")
+        assert len(element.find_elements_by_css_selector("div.sticker"))==1
+
